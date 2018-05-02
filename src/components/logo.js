8a427d12f3fa5e49a865d6a2eee560f46b4dec01
huntersfuntime <hunterorges@gmail.com>
2018-05-02 11:37:15 -0600

    Created logo component

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Logo = () => {
+        return (
+            <div className="logo-main">
+                <img src="/assets/ds_circle_logo.png" alt="daily smarty logo big" />
+            </div>
+        )
+}
+
+export default Logo;
