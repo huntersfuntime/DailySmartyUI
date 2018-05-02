6d56a674e196d9fec37bc24a97cc7eda3f6a8b8b
huntersfuntime <hunterorges@gmail.com>
2018-05-02 12:36:13 -0600

    Set up redux form in search bar component

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <Logo />
-        <SearchBar />
+        <SearchBar /> 
         <RecentPosts />
       </div>
     );
