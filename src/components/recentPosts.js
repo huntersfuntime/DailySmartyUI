14993faa15433b9832cfadf05a2d1fb23a964a10
huntersfuntime <hunterorges@gmail.com>
2018-05-02 12:10:54 -0600

    Created recent posts and search bar components

---
+++ src/components/recentPosts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const RecentPosts = () => {
+    return(
+        <div className="recent-posts">
+            <div className="recent-posts__wrapper">
+                <div className="recent-posts__heading">Recent Posts</div>
+                <ul className="recent-posts__posts">
+                    <li> Recent post 0 </li>
+                    <li> Recent post 1 </li>
+                    <li> Recent post 2 </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default RecentPosts;
